78b692e5c3264c9d1aa4224138e4dc6b2e34a88e
Masudphero~ <mdmasudrana8547@gmail.com>
2025-01-06 13:10:35 +0600

    routing setap

---
+++ src/router/router.jsx
@@ -0,0 +1,23 @@
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/home/Home";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      errorElement: <h2>Route not found</h2>,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        }
+      ]
+    },
+  ]);
+
+  export default router;
